347eee8cd483638c8d29bcdd0a1c4e6584582647
Lilleman <lilleman@larvit.se>
2016-07-22 15:00:00 +0200

    Made the minion-template a pub sub example

--- minion-template/index.js
+++ minion-template/index.js
@@ -5,25 +5,28 @@ function publisher(conn) {
 	conn.createChannel(function(err, ch) {
 		if (err) throw err;
 
+		ch.assertExchange('autobahn', 'fanout', {durable: false});
+
 		console.log('Sending message');
-		ch.assertQueue('queueName');
-		ch.sendToQueue('queueName', new Buffer('something to do'));
+		ch.publish('autobahn', '', new Buffer('Message content'));
 	});
 }
 
-// Consumer
-function consumer(conn) {
+// Subscriber
+function subscriber(conn, subNr) {
 	conn.createChannel(function(err, ch) {
 		if (err) throw err;
 
-		ch.assertQueue('queueName');
-		ch.consume('queueName', function(msg) {
-			if (msg !== null) {
-				console.log('Incoming message:');
-				console.log(msg);
-				//console.log(msg.content.toString());
-				ch.ack(msg);
-			}
+		ch.assertExchange('autobahn', 'fanout', {durable: false});
+
+		ch.assertQueue('', {exclusive: true}, function(err, q) {
+			ch.bindQueue(q.queue, 'autobahn', '');
+
+			ch.consume(q.queue, function(msg) {
+				console.log('nr ' + subNr + ' Received message!!! :D');
+				console.log(msg.content.toString());
+				console.log('------');
+			});
 		});
 	});
 }
@@ -33,6 +36,13 @@ require('amqplib/callback_api').connect('amqp://test:test@172.17.0.2/', function
 
 	console.log('Connected to server');
 
-	consumer(conn);
-	publisher(conn);
+	subscriber(conn, 1);
+	subscriber(conn, 2);
+	subscriber(conn, 3);
+
+	setInterval(function() {
+		console.log('publishing');
+		publisher(conn)
+	}, 2000);
+	;
 });
