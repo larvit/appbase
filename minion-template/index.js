b33aef87e9c530b0802709bc14e7b6e5343015b0
Lilleman <lilleman@larvit.se>
2016-07-23 12:08:26 +0200

    Made corresponding changes to the index.js file

--- minion-template/index.js
+++ minion-template/index.js
@@ -1,38 +1,9 @@
 'use strict';
 
-const	log	= require('winston');
+require(__dirname + '/models/logSetup.js');
 
 // Set current working directory
 process.cwd(__dirname);
-
-// Add support for daily rotate file
-log.transports.DailyRotateFile = require('winston-daily-rotate-file');
-
-// Handle logging from config file
-log.remove(log.transports.Console);
-try {
-	const logConf = require(__dirname + '/config/log.json');
-
-	for (const logName of Object.keys(logConf)) {
-		let logInstances = logConf[logName];
-
-		if (typeof logInstances !== Array) {
-			logInstances = [logInstances];
-		}
-
-		for (let i = 0; logInstances[i] !== undefined; i ++) {
-			log.add(log.transports[logName], logInstances[i]);
-		}
-	}
-} catch(err) {
-	log.add(log.transports.Console, {
-		'colorize':	true,
-		'timestamp':	true,
-		'level':	'info',
-		'json':	false
-	});
-	log.warn('Could not load log config from file "' + __dirname + '/config/log.json" falling back to console only');
-}
 
 // Application specific stuff below
 function amqpTest() {
